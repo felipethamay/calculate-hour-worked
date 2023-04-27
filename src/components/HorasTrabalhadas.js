7f91da9d62507bc1dcae1be5e610b9e1c3bbb56e
Felipe Thamay <felipethamay@hotmail.com>
2023-04-27 13:22:39 -0300

    incluido input mask

--- src/components/HorasTrabalhadas.js
+++ src/components/HorasTrabalhadas.js
@@ -1,73 +1,64 @@
 import React, { useState } from "react";
+import moment from 'moment';
+import InputMask from "react-input-mask";
 import "./HorasTrabalhadas.css";
 
 function HorasTrabalhadas() {
-  const [horaEntrada1, setHoraEntrada1] = useState("");
-  const [horaSaida1, setHoraSaida1] = useState("");
-  const [horaEntrada2, setHoraEntrada2] = useState("");
-  const [horaSaida2, setHoraSaida2] = useState("");
+  const [timeEntries, setTimeEntries] = useState([
+    { id: 1, start: '', end: '' },
+    { id: 2, start: '', end: '' }
+  ]);
 
-  const calcularHorasTrabalhadas = () => {
-    const entrada1 = new Date(`01/01/2022 ${horaEntrada1}`);
-    const saida1 = new Date(`01/01/2022 ${horaSaida1}`);
-    const entrada2 = new Date(`01/01/2022 ${horaEntrada2}`);
-    const saida2 = new Date(`01/01/2022 ${horaSaida2}`);
+  const addTimeEntry = () => {
+    const newId = timeEntries[timeEntries.length - 1].id + 1;
+    setTimeEntries([...timeEntries, { id: newId, start: '', end: '' }]);
+  };
 
-    const diferenca1 = Math.abs(saida1 - entrada1);
-    const diferenca2 = Math.abs(saida2 - entrada2);
-    const totalHoras = (diferenca1 + diferenca2) / (1000 * 60 * 60);
+  const handleTimeChange = (id, field, value) => {
+    setTimeEntries(prevEntries => prevEntries.map(entry => {
+      if (entry.id === id) {
+        return { ...entry, [field]: value };
+      }
+      return entry;
+    }));
+  };
 
-    return totalHoras.toFixed(2);
+  const calculateTotalHours = () => {
+    let totalHours = 0;
+    timeEntries.forEach(entry => {
+      const start = moment(entry.start, 'HH:mm');
+      const end = moment(entry.end, 'HH:mm');
+      if (start.isValid() && end.isValid()) {
+        const hours = moment.duration(end.diff(start)).asHours();
+        totalHours += hours;
+      }
+    });
+    return totalHours.toFixed(2);
   };
 
   return (
     <div className="container">
       <h1>Calculadora de Horas Trabalhadas</h1>
-      <div>
-        <label htmlFor="entrada1">Hora de Entrada 1:</label>
-        <input
-          type="time"
-          id="entrada1"
-          value={horaEntrada1}
-          onChange={(e) => setHoraEntrada1(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="saida1">Hora de Saída 1:</label>
-        <input
-          type="time"
-          id="saida1"
-          value={horaSaida1}
-          onChange={(e) => setHoraSaida1(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="entrada2">Hora de Entrada 2:</label>
-        <input
-          type="time"
-          id="entrada2"
-          value={horaEntrada2}
-          onChange={(e) => setHoraEntrada2(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="saida2">Hora de Saída 2:</label>
-        <input
-          type="time"
-          id="saida2"
-          value={horaSaida2}
-          onChange={(e) => setHoraSaida2(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <button className="calcular-btn" onClick={() => alert(`Horas trabalhadas: ${calcularHorasTrabalhadas()} horas`)}>
-          Calcular
-        </button>
-      </div>
+      {timeEntries.map(entry => (
+        <div key={entry.id}>
+          <label htmlFor={`start${entry.id}`}>Entrada:</label>
+          <InputMask
+            mask="99:99"
+            maskChar={null}
+            value={entry.start}
+            onChange={e => handleTimeChange(entry.id, 'start', e.target.value)}
+          />
+          <label htmlFor={`end${entry.id}`}>Saída:</label>
+          <InputMask
+            mask="99:99"
+            maskChar={null}
+            onChange={e => handleTimeChange(entry.id, 'end', e.target.value)}
+            value={entry.end}
+          />
+        </div>
+      ))}
+      <button onClick={addTimeEntry}>Adicionar Horário</button>
+      <h2>Total de Horas Trabalhadas: {calculateTotalHours()}</h2>
     </div>
   );
 }
